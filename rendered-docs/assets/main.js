987ad6590e3a36d26784e85e99c6bb2ccfe34ce2
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-02-08 07:25:00 -0600

    Update main.js

--- rendered-docs/assets/main.js
+++ rendered-docs/assets/main.js
@@ -1,6 +1,7 @@
 "use strict";
 document.onload = ()=>{
   [...document.querySelectorAll('a[href*="google-apps-script"] span,a[href*="google-apps-script"]:not(:has(*))')].filter(x=>x?.textContent?.includes?.('google-apps-script')).forEach(x=>(x.textContent=x.textContent.replace('google-apps-script.','')));
+  [...document.querySelectorAll('a[href*="variables/google-apps-script-events"]')].forEach(x=>{x.href=x.href.replace('variables','interfaces')});
   if(window!==window.top)[...document.querySelectorAll('a[href]:not(a[href^="http"])')].forEach(x=>x.setAttribute('href',x.href));
 }
 window.onload = document.onload;
